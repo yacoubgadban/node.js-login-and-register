3c4f89ef854f6cd3c043fc9e262aa45eefab9205
Yacoub Gadban <yacoub.gadban@gmail.com>
2021-11-19 15:00:09 +0200

    aa

--- app.js
+++ app.js
@@ -11,12 +11,13 @@ let user={isLoggedIn: false};
 const dbURI = "mongodb+srv://yacoub2:200748770@cluster0.sxa4m.mongodb.net/login?retryWrites=true&w=majority";
 mongoose.connect(dbURI,{useNewUrlParser:true ,useUnifiedTopology:true})
 .then((result)=>{console.log('***** Server and database connected ;) *****');
-const port=process.env.PORT ||3000;
-app.listen(port)
-app.use(compression())
+
 
 })
 .catch((err)=>{err.message})
+const port=process.env.PORT ||3000;
+app.listen(port)
+app.use(compression())
 
 app.set('view engine','ejs')
 app.use(express.static('public'))
