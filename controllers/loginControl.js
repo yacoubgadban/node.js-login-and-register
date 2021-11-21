59fe5199977008aae3f1f71084b48b2cedbcd189
Yacoub Gadban <yacoub.gadban@gmail.com>
2021-11-21 23:23:11 +0200

    validate

--- controllers/loginControl.js
+++ controllers/loginControl.js
@@ -7,9 +7,19 @@ var localStorage = require('local-storage');
 
 
 
+const GetLogin=async(req, res)=>{
+    localStorage.remove('joiError')
 
+    const joiError=await localStorage.get('joiError')
+
+    localStorage.remove('data')
+    const data=await localStorage.get('data')
+    
+    res.render('login',{joiError:joiError,data:data})
+}
 
 const loginFunc=async(req, res)=>{
+    
     const schema =Joi.object({
             
        
@@ -20,39 +30,45 @@ const loginFunc=async(req, res)=>{
     const  joiError = schema.validate(req.body)
 
 
-    const user = await User.findOne({email:req.body.email})
-
     if(joiError.error){
         
-        localStorage.set('joiError',joiError)
-        
-        
-    res.render('login',{joiError:joiError})
+    localStorage.set('joiError',joiError)
+   localStorage.set('data',req.body)   
+   const data=await localStorage.get('data')
+
+    res.render('login',{joiError:joiError,data:data})
+
     }
+    const user = await User.findOne({email:req.body.email})
 
-   else if(!user){
-       console.log('wrong email')
-   res.redirect('/login')
-   }
-   const checkPassword=await bcrypt.compare(req.body.password ,user.password)
+     if(!user){
 
+   localStorage.set('data',req.body)   
+   const data=await localStorage.get('data')
+   const joiError= {error:{message:"Email or Password is not correct !"}}
+    res.render('login',{joiError:joiError,data:data})
+
+  }
+   
+    const checkPassword=await bcrypt.compare(req.body.password ,user.password)
    if(!checkPassword)    
    {
-       console.log('wrong password')
-       res.redirect('/login')
-   }else{
-          
-    user.isLoggedIn=true
-       await user.save()
-       console.log('success')
-       res.redirect('/user/'+""+user._id+"")}
-}
-const GetLogin=async(req, res)=>{
-    localStorage.remove('joiError')
-    const joiError= localStorage.get('joiError')
+    localStorage.set('data',req.body)   
+    const data=await localStorage.get('data')
+    const joiError= {error:{message:"Email or Password is not correct !"}}
 
-    res.render('login',{joiError:joiError})
+    res.render('login',{joiError:joiError,data:data})
+   }
+            else{
+                    
+                user.isLoggedIn=true
+                await user.save()
+                res.redirect('/user/'+""+user._id+"")
+                }
 }
+
+
+
 module.exports={
     GetLogin,
     loginFunc,
--- controllers/registerControl.js
+++ controllers/registerControl.js
@@ -1,24 +1,56 @@
 const express = require('express')
 const User = require('../models/user')
 const bcrypt = require('bcrypt')  
+const Joi = require('joi');
+
+var localStorage = require('local-storage');
 
 
-const GetRegister=(req, res)=>{
-    
-    res.render('register')
+const GetRegister=async(req, res)=>{
+    localStorage.remove('joiErrorR')
+    const joiErrorR= await localStorage.get('joiErrorR')
+
+    localStorage.remove('data')
+    const data=await localStorage.get('data')
+    res.render('register',{joiErrorR:joiErrorR ,data:data})
 }
 
 const RegisterFunc=async(req, res)=>{
-    
-    const user=await User.findOne({email:req.body.email})
-    if(req.body.password!=req.body.password2){
+    const schema =Joi.object({
+            
+        fname: Joi.string().alphanum().min(2).max(10).required(),
+        lname: Joi.string().alphanum().min(2).max(10).required(),
+        email: Joi.string().min(6).max(30).email({ minDomainSegments: 2}).required(),
+        password: Joi.string().min(6).max(20).required(),
+        password2: Joi.string().min(6).max(20).required(),
 
-       console.log('err')
+       
+    })
+    const  joiErrorR = schema.validate(req.body)
+    const user=await User.findOne({email:req.body.email})
+
+    if(joiErrorR.error){
+        
+        localStorage.set('joiErrorR',joiErrorR)  
+        localStorage.set('data',req.body)   
+        const data=await localStorage.get('data')            
+        res.render('register',{joiErrorR:joiErrorR,data:data})
+        }
+       
+    else if(req.body.password!=req.body.password2){
+
+       localStorage.set('data',req.body)
+       const data=await localStorage.get('data')            
+       const joiErrorR= {error:{message:"Password is not same !"}}
+
+       res.render('register',{joiErrorR:joiErrorR,data:data})
 
 
     }else if(user){
-        console.log('user is already ')
-        res.redirect('/register')
+        localStorage.set('data',req.body)
+        const data=await localStorage.get('data')            
+        const joiErrorR= {error:{message:"This email is already created !"}}
+        res.render('register',{joiErrorR:joiErrorR,data:data})
 
     }
     else{
